2095a7b96bad604a4984333db56b81bb0582384e
Mikiyas Meles Assefa <massefa@andrew.cmu.edu>
2025-08-05 19:32:57 +0300

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import { Analytics } from '@vercel/analytics/react'
+import { Suspense } from 'react'
 
 export const metadata: Metadata = {
   title: 'Yegna Write',
@@ -16,7 +17,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {children}
+        <Suspense fallback={null}>
+          {children}
+        </Suspense>
         <Analytics />
       </body>
     </html>
